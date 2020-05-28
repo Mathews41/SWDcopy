a731bb19eb29bbd62c09169ce1d7543f766da332
Mathews41 <marklmathews4@gmail.com>
2020-05-28 09:57:46 -0700

    first commit

---
+++ swdurethane/src/components/Header.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import icon1 from '../images/icon3.png'
+import {Link} from 'gatsby'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Toggle from 'react-bootstrap/DropdownToggle'
+import Item from 'react-bootstrap/DropdownItem'
+import Menu from 'react-bootstrap/DropdownMenu'
+import DropdownMenu from 'react-bootstrap/DropdownMenu'
+
+
+
+export default function Header() {
+  
+  const [isShown, setIsShown] = useState(true);
+  return (
+  <header id="header">
+    <div className='superheaderContainer'>
+      
+    <Link className='superheader' to="/contact/"> Contact Us 800-828-1394</Link>
+    </div>
+      <nav className="navbar">
+        <div className='architectsMenu'>
+          
+        </div>
+        <div className='industrialMenu'>
+          
+        </div>
+        <div className="navbar-brand" href="/">
+          <a href='/'>
+            <img className='icon1' src={icon1} alt="" href='/' />
+          </a>
+          <div className='subtext'>
+            <h3 className='icon-text'to='/'>
+              <a href='/'>Polyurethane Technologies
+              </a>
+            </h3>
+            <sub className='subsubtext'>
+            Industrial and Construction Products
+            </sub>
+          </div>
+        </div>
+          <ul className='top-nav-list' >
+            <li>
+              <div className='products'>
+              <Link 
+              className='linkProd' to='/products/'
+              > Products
+              <div className='productsMenu'>
+                  <ul>
+                    <li>
+                      <Link className='prodLink' to='/products/'>Wall Insulation</Link>
+                    </li>
+                    <li>
+                      <Link className='prodLink' to='/products/'>Wall Insulation</Link>
+                    </li>
+                    <li>
+                      <Link className='prodLink' to='/products/'>Wall Insulation</Link>
+                    </li>
+                    <li>
+                      <Link className='prodLink' to='/products/'>Wall Insulation</Link>
+                    </li>
+                    <li>
+                      <Link className='prodLink' to='/products/'>Wall Insulation</Link>
+                    </li>
+                  </ul>
+                </div>
+              </Link>
+                </div>
+            </li>
+            <li>
+              <Link className='linkCont' to='/contractors/'
+              >Contractors</Link>
+            </li>
+            <li>
+              <div className='dropdown'>
+              <Link className='linkArch' to='/architectsStepper/'>
+                Architects
+                </Link>
+                <div className='architectsMenu'>
+                  <ul>
+                    <li>
+                      <Link className='archLink' to='/architectsInsulation/'>Wall Insulation</Link>
+                    </li>
+                    <li className='roofList'>
+                    <Link className='roofLink' to='/architectsRoofing/'>Roofing Systems</Link>
+                    </li>
+                  </ul>
+                </div>
+                </div>
+            </li>
+            <li>
+              <Link className='linkProj'  to='/projects/'>Projects</Link>
+            </li>
+            <li>
+              <Link className='linkInd' to='/industrial/'>Industrial</Link>
+            </li>
+            <li>
+              <Link className='linkAbout' to='/about/'>About</Link>
+            </li>
+          </ul>
+      </nav>
+</header>
+  )
+}
+
---
+++ swdurethane/src/pages/index.js
@@ -0,0 +1,190 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+import Header from '../components/Header'
+import Main from '../components/Main'
+import Footer from '../components/Footer'
+import { graphql } from 'gatsby'
+import { Carousel } from 'react-bootstrap'
+
+//images
+import bg1 from '../images/bg1.jpg'
+import bg4 from '../images/bg4.jpg'
+import labshot from '../images/Lab Shot 1 small.png'
+import icon2 from '../images/icon2.png'
+import ASU from '../images/ASU.jpg'
+import uofahonors from '../images/UofAHonors.jpg'
+
+
+import PropTypes from 'prop-types'
+
+
+const propTypes = {
+  pause: PropTypes.oneOf(['hover', false])
+}
+
+
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isArticleVisible: false,
+      timeout: false,
+      articleTimeout: false,
+      article: '',
+      loading: 'is-loading',
+      width: 0,
+    }
+    this.handleOpenArticle = this.handleOpenArticle.bind(this)
+    this.handleCloseArticle = this.handleCloseArticle.bind(this)
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount () {
+    this.timeoutId = setTimeout(() => {
+        this.setState({loading: ''});
+    }, 100);
+    document.addEventListener('mousedown', this.handleClickOutside);
+
+    this.handleWindowSizeChange() // Set width
+    window.addEventListener('resize', this.handleWindowSizeChange)
+  }
+  
+  componentWillUnmount () {
+    if (this.timeoutId) {
+        clearTimeout(this.timeoutId);
+    }
+    document.removeEventListener('mousedown', this.handleClickOutside);
+
+    window.removeEventListener('resize', this.handleWindowSizeChange)
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth })
+  }
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+  handleOpenArticle(article) {
+
+    this.setState({
+      isArticleVisible: !this.state.isArticleVisible,
+      article
+    })
+
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout
+      })
+    }, 325)
+
+    setTimeout(() => {
+      this.setState({
+        articleTimeout: !this.state.articleTimeout
+      })
+    }, 350)
+
+  }
+
+  handleCloseArticle() {
+
+    this.setState({
+      articleTimeout: !this.state.articleTimeout
+    })
+
+    setTimeout(() => {
+      this.setState({
+        timeout: !this.state.timeout
+      })
+    }, 325)
+
+    setTimeout(() => {
+      this.setState({
+        isArticleVisible: !this.state.isArticleVisible,
+        article: ''
+      })
+    }, 350)
+
+  }
+
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      if (this.state.isArticleVisible) {
+        this.handleCloseArticle();
+      }
+    }
+  }
+
+  render(props) {
+    const { width } = this.state;
+    const isMobile = width <= 600;
+    console.log(width, ' px')
+    const {data} = this.props;
+    return (
+      <Layout location={this.props.location}>
+        <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
+          <div id="wrapper">
+            <div className='carContainer'>
+            <Carousel data-pause='false' className='carousel'>
+              <Carousel.Item className='item'>
+                <img className='car-img' src={bg1} alt= 'this is the first slide'/>
+                <Carousel.Caption className='caption'>
+                  <h3>St. Joseph Hospital</h3>
+                  <h5 className='carTag'>Denver, Colorado</h5>
+                  <button href='/projects/' id='button'><h3>Learn More</h3></button>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className='item'>
+                <img className='car-img' src={uofahonors} alt= 'this is the first slide'/>
+                <Carousel.Caption className='caption'>
+                  <h3> University of Arizona Honors Student Housing
+                  </h3>
+            <h5 className='carTag'>Tuscon, Arizona</h5>
+                  <button href='/projects/' id='button'><h3>Learn More</h3></button>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className='item'>
+                <img className='car-img' src={ASU} alt= 'this is the first slide'/>
+                <Carousel.Caption className='caption'>
+                  <h3>Arizona State University Greek Leadership Student Housing </h3>
+                  <h5 className='carTag'>Tempe, Arizona</h5>
+                  <button href='/projects/' id='button'><h3>Learn More</h3></button>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item className='item'>
+              <img className='car-img' src={bg4} alt= 'this is the second slide'/>
+              <Carousel.Caption className='caption'>
+              <h3>The Church of Jesus Christ of Latter-day Saints Temple</h3>
+              <h5 className='carTag'>Gilbert, Arizona</h5>
+                  <button href='/projects/'id='button'><h3>Learn More</h3></button>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+            <div className='lpImgContainer'>
+            <img alt='swd chemical lab shot'className='secondaryimg' src={labshot}/>
+            <img className='lplogo' src={icon2}/>
+            </div>
+            </div>
+            <div className='lp2'>
+              <h3> Learn From The Polyurethane Experts On Our YouTube Channel! </h3>
+              <div className='videoCont'>
+            <iframe width="424" height="238" src="https://www.youtube.com/embed/PzoNv7RF-Kc?allowfullscreen=true" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="1280" height="720" src="https://www.youtube.com/embed/pMJPh2sd-uc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="270" height="152" src="https://www.youtube.com/embed/DsgfZ3lC6XY" frameborder="0" allowfullscreen></iframe>
+            </div>
+            </div>
+            <Header/>
+            <Main/>
+            <Footer/>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default IndexPage
+
